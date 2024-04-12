57016ee7d9d2883bf100294d69ce60d5e6379c7d
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-13 00:59:58 +0700

    Update 1_Main.py

--- 1_Main.py
+++ 1_Main.py
@@ -95,12 +95,9 @@ def main():
                 gemini_api_input()
         tab1, tab2, tab3 = st.tabs(["📥 Input", "📖 Story", "💬 Chat"])
 	
-        with tab1:
-		if_key_in_session_state(tab_input)
-        with tab2:
-                if_key_in_session_state(tab_story)
-        with tab3:
-                if_key_in_session_state(tab_chat)
+        with tab1: if_key_in_session_state(tab_input)
+        with tab2: if_key_in_session_state(tab_story)
+        with tab3: if_key_in_session_state(tab_chat)
 
 # Execute main
 main()
