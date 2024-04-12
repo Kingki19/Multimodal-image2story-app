6d6dd31854ca6f44d41164d1b9cfcff072988459
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-12 09:10:03 +0700

    Update 1_Main.py

--- 1_Main.py
+++ 1_Main.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
 # App title
-st.title('MI2S')
+st.title('MI2S: Multimodal Image-2-Stories using Gemini-AI')
 st.divider()
 
 # Description
 st.markdown("""
-This is description on Markdown
+MI2S (Multimodal Image2Stories) is an innovative application designed to transform images into captivating narratives.  
+This cutting-edge tool utilizes multimodal technology, combining visual and textual elements to generate short stories or even full-length novels based on input images.   
+By leveraging Gemini-AI, MI2S analyzes the content, context, and emotions conveyed in the image to craft immersive and engaging storytelling experiences.  
+Whether you're seeking to create compelling short stories or embark on novel-writing adventures, MI2S opens up endless possibilities for creative expression through the fusion of visual and literary arts. 
 """)  
 
 # Input API key for Gemini API
