1f2680268315acf97b04cba025f7e9b67225fd6d
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-15 13:08:51 +0700

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.title('About')
+st.divider()
+
+st.header('Brief Description')
+st.markdown('''
+**MI2S** was an app that use Gemini-AI behind it. It use images then generate stories. There's a lot of model that already use it, but the novelty of this app is:
+
+1. It use multimodal model like Gemini-AI
+2. It can input image continuously (my expectation)
+
+I would give more detail explanation in Documentation.
+''')
+st.divider()
+
+st.header('Developer')
+st.markdown('''
+- Muhammad Rizqi: 
+Rizqi was a lonely. He can't make any friends longer than Python compiler. That's why he make this app alone. :(
+''')
+st.divider()
