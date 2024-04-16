02a86b0d4d3e21c05fcd61b0e504d1a947b296bc
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-16 05:57:15 -0700

    Update 2_Documentation.py

--- pages/2_Documentation.py
+++ pages/2_Documentation.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('MI2S Documentation')
+def how_to_use():
+        
+def main():
+        st.title('MI2S Documentation')
+        st.divider()
+        st.header('How to use')
+        st.divider()
+        st.header('graph input-process-output')
+        st.divider()
