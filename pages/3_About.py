3da7b35c494611ebba7131cd1dfbe61e79ee9601
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-27 23:58:34 -0700

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -18,10 +18,10 @@ st.header('Contributors')
 st.markdown('''
 - Muhammad Rizqi (Kingki19):  
 Kingki was a loner. He can't make any friends longer than Python compiler. That's why he make this app alone. I hope you enjoy my app.
-(GITHUB | KAGGLE | YOUTUBE)
+([GITHUB](https://github.com/Kingki19) | [KAGGLE](https://www.kaggle.com/kingki19) | [YOUTUBE](https://www.youtube.com/@kingki1953/featured) | [LINKEDIN](https://www.linkedin.com/in/muhammad-rizqi-921538248/))
 
 - Muhammad Azka Nabhan Sauqi (Aceng):  
 Aceng help me to make the video about this.
-([YOUTUBE](https://youtube.com/@azkanabhan4658?si=4vaLtxatGwAt1huf))
+([YOUTUBE](https://youtube.com/@azkanabhan4658?si=4vaLtxatGwAt1huf) | [LINKEDIN](https://linkedin.com/in/azkanabhan/))
 ''')
 st.divider()
