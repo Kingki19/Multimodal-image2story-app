fe0fa8313cd305e3823ab037e347630411698b86
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-28 03:09:07 -0700

    Update 2_Documentation.py

--- pages/2_Documentation.py
+++ pages/2_Documentation.py
@@ -7,7 +7,7 @@ def main():
         st.header('Feature:')
         st.divider()
         st.header('Graph Input-Output Process')
-        graph_url = 'https://github.com/Kingki19/Multimodal-image2story-app/blob/main/images/I_O%20graph.drawio.png'
+        graph_url = 'https://github.com/Kingki19/Multimodal-image2story-app/blob/main/images/I_O%20graph.drawio.png?raw=true'
         st.image(graph_url, caption='Graph I/O')
         st.divider() 
 
