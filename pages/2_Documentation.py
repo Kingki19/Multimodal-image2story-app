4ae48dd45c2c944767952bc25aee515ab41b0d97
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-28 03:08:05 -0700

    Update 2_Documentation.py

--- pages/2_Documentation.py
+++ pages/2_Documentation.py
@@ -7,22 +7,8 @@ def main():
         st.header('Feature:')
         st.divider()
         st.header('Graph Input-Output Process')
-        code = """
-        graph TB
-                api_key[/User input api key/]
-                api_key_bool{API-key exist or not?}
-                input_variable[/User input image, writing style, etc/]
-                api_key --> api_key_bool
-                api_key_bool -- API key doesn't exist, input again --> api_key
-                api_key_bool -- API key exist, continue --> input_variable
-
-                generate_story(Generate a story)
-                story_result[/Story result/]
-                input_variable --> generate_story --> story_result
-
-                
-        """
-        stmd.st_mermaid(code)
+        graph_url = 'https://github.com/Kingki19/Multimodal-image2story-app/blob/main/images/I_O%20graph.drawio.png'
+        st.image(graph_url, caption='Graph I/O')
         st.divider() 
 
 main()
