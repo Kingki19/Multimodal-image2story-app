6c6ad2055ac6fca0bacb92be5f7984697320634c
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-04-28 03:24:32 -0700

    Update 2_Documentation.py

--- pages/2_Documentation.py
+++ pages/2_Documentation.py
@@ -4,7 +4,20 @@ import streamlit_mermaid as stmd
 def main():
         st.title('MI2S Documentation')
         st.divider()
-        st.header('Feature:')
+        st.header('Feature')
+        st.markdown("""
+        - It can generate any text/story/poem/etc from image
+        - It can generate story continously
+        - Chat with story: you can ask what really happen to story and ask Gemini suggestion what's next story that could be make
+        """)
+        st.divider()
+        st.header('Weakness/Problems')
+        st.markdown("""
+        - Chat feature still not develop yet (I don't have enough time)
+        - User can't continue story if the user end it (suggestion: User can send document that contain last story input. But i don't have enough time to develop it)
+
+        *Main feature is complete and i will publish the project to Google AI Hackaton. After that, i still continue the project until all is done.*
+        """)
         st.divider()
         st.header('Graph Input-Output Process')
         graph_url = 'https://github.com/Kingki19/Multimodal-image2story-app/blob/main/images/I_O%20graph.drawio.png?raw=true'
